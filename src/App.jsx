194d384c876a2e77cdff57c9277c7892a1ac4f9b
kongtup Tangsatjatham <kongtupjai@gmail.com>
2025-12-14 19:02:52 +0700

    Update header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
+    <div className="flex min-h-svh flex-col">
+      <Header></Header>
+      <main className="flex flex-1 items-center justify-center">
+        <div className="flex flex-col border border-black-100 rounded-sm px-15 py-20">
+          <Input
+            type="email"
+            placeholder="Email"
+            className="h-8 px-2 py-1 text-sm w-64"
+          />
+          <Input
+            type="text"
+            placeholder="Password"
+            className="h-8 px-2 py-1 text-sm w-64 mt-5"
+          />
+          <Button className="mt-10">Submit</Button>
+        </div>
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <div className="bg-black h-17 flex items-center tracking-wide">
+      <h1 className="font-bbh text-white ml-12 hover:text-blue-400 transition-colors duration-300">
+        JAIJAI-Log-in page
+      </h1>
+    </div>
+  );
+}
